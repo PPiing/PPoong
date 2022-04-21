e07010396cdf689fb93efedc0579bd56855a6e76
joohongpark <daleaderprofq@gmail.com>
2022-04-21 18:29:51 +0900

    :hammer: #10 Add ESLint rule

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export default class AppService {
-  // eslint-disable-next-line class-methods-use-this
   getHello(): string {
     return 'Hello World!';
   }
