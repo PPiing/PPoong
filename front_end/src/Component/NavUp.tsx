726bded44c969a14dec26dd02a8f3d97c6e0d6bd
HyeonsikBae <baehyeonsik@gmail.com>
2022-04-24 16:18:19 +0900

    :sparkles #13 Create react-ts project

---
+++ front_end/src/Component/NavUp.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function NavUp() {
+  return (
+    <div
+      className="nav-up"
+      style={{
+        backgroundColor: "rgb(255, 0, 255)",
+        height: "100px",
+        width: "100%",
+        position: "fixed",
+        top: "0",
+        left: "0",
+      }}
+    >
+      NAV_UP
+    </div>
+  );
+}
+
+export default NavUp;
